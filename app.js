50bed19351ff924ffb1d185da0e67af731fa094e
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2024-02-19 23:12:41 -0400

    update port in app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3030;
+/*const port = 3030;*/
+const port = process.env.PORT || 3001;
 const path = require("path");
 
 /* Configuraciones*/
